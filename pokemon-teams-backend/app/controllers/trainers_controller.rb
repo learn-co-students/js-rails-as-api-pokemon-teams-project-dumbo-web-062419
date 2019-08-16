6fbf40e10a4d910d48a1f5539dc90e40275e9273
lgm527 <laurellmccaffrey@gmail.com>
2019-08-15 22:27:27 -0400

    gotta catch em all

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,14 @@
+class TrainersController < ApplicationController
+
+  def index
+    @trainers = Trainer.all
+    render json: @trainers
+  end
+
+  def show
+    @trainer = Trainer.find(params[:id])
+    @pokemons = @trainer.pokemons
+    render json: @pokemons
+  end
+  
+end
